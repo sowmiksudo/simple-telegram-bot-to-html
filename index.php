f0d279c40a6586f58c22b1ce47b38048c853e1fe
Shayer Mahmud Sowmik <69670910+Ign0r3dH4x0r@users.noreply.github.com>
2022-07-31 11:07:28 +0600

    Removed bot token

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'https://api.telegram.org/bot5443700122:AAFYxyGEN3dCd66QVjaVGEmrZBBsmcFioBs/getupdates');
+curl_setopt($ch, CURLOPT_URL, 'https://api.telegram.org/{bot_token}/getupdates');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
 
